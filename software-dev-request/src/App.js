b283e8c126d09d846fffaf02ca83a13f564df69d
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-19 12:05:44 +0000

    ui update

--- software-dev-request/src/App.js
+++ software-dev-request/src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import Snowfall from 'react-snowfall'; // Import the snowfall component
+import Snowfall from 'react-snowfall';
 import './App.css';
 
 const App = () => {
-  const [services] = useState([
+  const servicesData = [
     { name: 'Code Review', price: 100 },
     { name: 'Coding', price: 300 },
     { name: 'Pull Request', price: 500 },
-  ]);
+  ];
 
   const [selectedServices, setSelectedServices] = useState([]);
   const [gitUrl, setGitUrl] = useState('');
+  const [description, setDescription] = useState('');
   const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleServiceChange = (serviceName) => {
     setSelectedServices((prevServices) => {
@@ -22,6 +24,17 @@ const App = () => {
     });
   };
 
+  const handleGitUrlChange = (url) => {
+    setGitUrl(url);
+    setError(false);
+    setSuccess(false); // Clear success message when Git URL changes
+  };
+
+  const totalCost = selectedServices.reduce((total, serviceName) => {
+    const service = servicesData.find((s) => s.name === serviceName);
+    return total + (service ? service.price : 0);
+  }, 0);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -30,29 +43,30 @@ const App = () => {
       return;
     } else {
       setError(false);
+      setSuccess(true); // Set success message
     }
 
-    console.log('Selected Services:', selectedServices);
-    console.log('Git URL:', gitUrl);
+    const dataToStore = {
+      gitUrl: gitUrl,
+      selectedServices: selectedServices,
+      description: description,
+    };
+
+    console.log('Data to store:', dataToStore);
+    // You can send dataToStore to a server, save it to a file, or use any desired storage mechanism
+
     // Add logic for payment processing here
   };
 
-  const totalCost = selectedServices.reduce((total, serviceName) => {
-    const service = services.find((s) => s.name === serviceName);
-    return total + (service ? service.price : 0);
-  }, 0);
-
   return (
     <div className="App">
-      <Snowfall snowflakeCount={100} // Customize snowflake count
-                snowflakeSize={[5, 10]} // Customize snowflake size range
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} />
+      <Snowfall snowflakeCount={100} snowflakeSize={[5, 10]} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} />
       <div className="form-container">
         <h1 className="app-title">Code Review App</h1>
         <form onSubmit={handleSubmit}>
           <div className="services-container">
             <h2>Select Services:</h2>
-            {services.map((service) => (
+            {servicesData.map((service) => (
               <label key={service.name} className="service-label">
                 <input
                   type="checkbox"
@@ -71,19 +85,39 @@ const App = () => {
               type="text"
               className="git-input"
               value={gitUrl}
-              onChange={(e) => setGitUrl(e.target.value)}
+              onChange={(e) => handleGitUrlChange(e.target.value)}
+            />
+            {error && (
+              <p className="error-message">Git URL is required</p>
+            )}
+          </div>
+          <div>
+            <h2>Description:</h2>
+            <textarea
+              className="description-input"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              style={{ width: '100%', height: '120px' }} // Adjust the width and height as needed
             />
           </div>
           <div className="total-cost">
-            Total Cost: ${totalCost}
+            <h2>Total Cost:</h2>
+            <p className="cost">${totalCost}</p>
           </div>
           <button type="submit" className="purchase-button">
             Purchase
           </button>
-          {error && (
-            <div className="popup">
-              <p className="error-message">Git URL is required</p>
-            </div>
+          {success && (
+            <p className="success-message">
+              <span className="highlight">Success!</span> You have purchased the following checkboxes for ${totalCost} dollars:
+              {selectedServices.map((serviceName) => (
+                <span key={serviceName}> {serviceName},</span>
+              ))}
+              <br />
+              URL: {gitUrl}
+              <br />
+              Description: {description}
+            </p>
           )}
         </form>
       </div>
