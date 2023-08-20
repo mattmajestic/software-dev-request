32e4536719e100b8d9265e681ae08aa533f7cd8f
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-20 04:02:49 +0000

    test

--- server/index.js
+++ server/index.js
@@ -12,13 +12,13 @@ app.use(express.json());
 
 app.post('/save-data', (req, res) => {
     try {
-      const dataFilePath = './data.json';
-      const currentData = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+      const dataFilePath = 'data.json';
+      const currentData = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
   
       const newData = req.body;
       currentData.push(newData);
   
-      fs.writeFileSync('./data.json', JSON.stringify(currentData, null, 2));
+      fs.writeFileSync('data.json', JSON.stringify(currentData, null, 2));
       
       res.status(200).json({ message: 'Data saved successfully' });
     } catch (error) {
@@ -32,7 +32,7 @@ app.post('/save-data', (req, res) => {
 app.get('/get-data', async (req, res) => {
   try {
     const data = await readData();
-    res.status(200).json(data.data);
+    res.status(200).json(data);
   } catch (error) {
     console.error('Error reading data:', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -41,7 +41,7 @@ app.get('/get-data', async (req, res) => {
 
 async function readData() {
   try {
-    const data = await fs.readFile('./data.json', 'utf-8');
+    const data = await fs.readFile('data.json', 'utf-8');
     return JSON.parse(data);
   } catch (error) {
     if (error.code === 'ENOENT') {
@@ -52,7 +52,7 @@ async function readData() {
 }
 
 async function saveData(data) {
-  await fs.writeFile('./data.json', JSON.stringify(data, null, 2));
+  await fs.writeFile('data.json', JSON.stringify(data, null, 2));
 }
 
 app.listen(PORT, () => {
