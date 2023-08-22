70be3134fe045301fba5c81d62d58474fd59d8da
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-22 00:42:20 +0000

    netlify test

--- software-dev-request/src/App.js
+++ software-dev-request/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Snowfall from 'react-snowfall';
-import Web3 from 'web3';
 import './App.css';
 
 const App = () => {
