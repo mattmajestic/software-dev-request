60cc96e09f685b633ca2330b743106e0c6309ac0
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-26 23:25:16 +0000

    tshoot

--- software-dev-request/src/App.js
+++ software-dev-request/src/App.js
@@ -62,6 +62,7 @@ const App = () => {
           {/* Description input */}
           <PurchaseComponent
             selectedServices={selectedServices}
+            totalCost={totalCost}
             onSuccess={handlePurchaseSuccess}
           />
           {success && (
