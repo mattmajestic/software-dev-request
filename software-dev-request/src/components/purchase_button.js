6868250dd2d31f4938cb838ba3a53814894bbc3f
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-27 16:50:21 +0000

    purchase button

--- software-dev-request/src/components/purchase_button.js
+++ software-dev-request/src/components/purchase_button.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { v4 as uuidv4 } from 'uuid';
 import '../App.css';
 
 const supabase = createClient('https://rjmgkgtoruefbqqohelw.supabase.co', process.env.REACT_APP_SUPABASE);
 
-const PurchaseComponent = ({ selectedServices, gitUrl, description, onSuccess }) => {
+const PurchaseComponent = ({ selectedServices, gitUrl, description }) => {
+  const [isPurchaseClicked, setIsPurchaseClicked] = useState(false);
   const purchaseId = uuidv4();
 
   const handlePurchase = async () => {
@@ -24,7 +25,7 @@ const PurchaseComponent = ({ selectedServices, gitUrl, description, onSuccess })
         console.error('Error saving purchase:', error);
       } else {
         console.log('Purchase saved successfully:', data);
-        onSuccess();
+        setIsPurchaseClicked(true); // Set the purchase clicked state to true
       }
     } catch (error) {
       console.error('Error saving purchase:', error);
@@ -38,10 +39,12 @@ const PurchaseComponent = ({ selectedServices, gitUrl, description, onSuccess })
         Purchase
       </button>
 
-      {/* Display the success message */}
-      <p className="success-message">
-        Your purchase with ID {purchaseId} has been completed.
-      </p>
+      {/* Display the success message only if the purchase button is clicked */}
+      {isPurchaseClicked && (
+        <p className="success-message">
+          Your purchase with ID {purchaseId} has been completed.
+        </p>
+      )}
     </div>
   );
 };
