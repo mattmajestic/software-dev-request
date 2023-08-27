7a9c125bf96c5e66653ee508e01cb2adc0230b95
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-27 02:05:08 +0000

    success message

--- software-dev-request/src/components/purchase_button.js
+++ software-dev-request/src/components/purchase_button.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import Modal from 'react-modal'; // Import the modal component
 import '../App.css';
 
 const supabase = createClient('https://rjmgkgtoruefbqqohelw.supabase.co', process.env.REACT_APP_SUPABASE);
 
 const PurchaseComponent = ({ selectedServices, totalCost, gitUrl, description, onSuccess }) => {
-  const [showPopup, setShowPopup] = useState(false);
+  const [requestSent, setRequestSent] = useState(false);
 
   const handlePurchase = async () => {
     try {
@@ -23,7 +22,7 @@ const PurchaseComponent = ({ selectedServices, totalCost, gitUrl, description, o
         console.error('Error saving purchase:', error);
       } else {
         console.log('Purchase saved successfully:', data);
-        setShowPopup(true); // Show the modal
+        setRequestSent(true); // Set the request sent state to true
         onSuccess(); // Call the success handler
       }
     } catch (error) {
@@ -38,20 +37,7 @@ const PurchaseComponent = ({ selectedServices, totalCost, gitUrl, description, o
         Purchase (${totalCost})
       </button>
       
-      <Modal
-        isOpen={showPopup}
-        onRequestClose={() => setShowPopup(false)} // Close the modal when requested
-        contentLabel="Popup Modal"
-        className="popup-modal"
-        overlayClassName="popup-overlay"
-      >
-        <div className="popup-content">
-          <p className="popup-message">Your request has been stored in the database.</p>
-          <button className="popup-close-button" onClick={() => setShowPopup(false)}>
-            Close
-          </button>
-        </div>
-      </Modal>
+      {requestSent && <p className="success-message">Your request was sent.</p>}
     </div>
   );
 };
